38452ad98b45780b001110a850ded8991e2a00f5
Artyom Rybalko <artyomrybalko@Air-Artyom.Dlink>
2024-07-25 12:22:11 +0300

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mergeConflictCourse
+//
+//  Created by Artyom Rybalko on 25.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
